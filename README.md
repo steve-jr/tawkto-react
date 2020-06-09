21c89079b4235900d7b86ba5ef4644c9cdaf5f37
Ibeh Stephen <steve.junior1230@gmail.com>
2020-06-09 11:42:50 +0000

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tawkto-react
 Integrate tawk.to into react application
+
+import {tawkTo} from 'tawkto-react'
+
+//acquire id by registering with tawk.to
+const propertyId = 'skjdbcjskjdcsdc'
+
+tawTo(propertyId)
+
+#Function must after the DOM is loaded, such as inside the componentDidMount
---
+++ index.js
@@ -0,0 +1,31 @@
+
+//pass your tawk propertyId
+export function tawkTo(propertyId){
+
+    if (!window) {
+        throw new Error('DOM is unavailable')
+    }
+
+    window.Tawk_API       = window.Tawk_API || {};
+    window.Tawk_LoadStart = new Date();
+
+    const tawk = document.getElementById('tawkId');
+    if (tawk) {
+        // Prevent TawkTo to create root script if it already exists
+        return window.Tawk_API;
+    }
+
+    const script = document.createElement("script");
+    script.id    = 'tawkId';
+    script.async = true;
+    script.src   = 'https://embed.tawk.to/' + propertyId + '/default';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+
+    const first_script_tag = document.getElementsByTagName("script")[0];
+    if (!first_script_tag || !first_script_tag.parentNode) {
+        throw new Error('DOM is unavailable')
+    }
+
+    first_script_tag.parentNode.insertBefore(script, first_script_tag)
+}
